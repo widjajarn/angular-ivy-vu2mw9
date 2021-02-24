5fcf2315fe019b486938085eb2549378b4d82233
widjajarn <widjajarn@gmail.com>
2021-02-24 16:49:26 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   public mydiv: HTMLDivElement;
-  public baris: HTMLInputElement; 
-  public kolom: HTMLInputElement; 
+  public textboxgiliran: HTMLInputElement;
+  public baris: HTMLInputElement;
+  public kolom: HTMLInputElement;
 
-  public giliran: number = 1; 
+  public giliran: number = 1;
   public papan = new Array(5).fill(0).map(() => new Array(5).fill("*"));
   public name = "Angular " + VERSION.major;
 
@@ -18,17 +19,21 @@ export class AppComponent {
     this.mydiv = document.getElementById("kotak") as HTMLDivElement;
     this.baris = document.getElementById("tbaris") as HTMLInputElement;
     this.kolom = document.getElementById("tkolom") as HTMLInputElement;
+    this.textboxgiliran = document.getElementById(
+      "tgiliran"
+    ) as HTMLInputElement;
     this.showpapan();
   }
 
   seticon() {
-    var brs = parseInt(this.baris.value); 
-    var klm = parseInt(this.kolom.value); 
+    var brs = parseInt(this.baris.value);
+    var klm = parseInt(this.kolom.value);
     this.papan[brs][klm] = this.giliran;
-    this.showpapan(); 
+    this.showpapan();
   }
 
   showpapan() {
+    this.textboxgiliran.value = this.giliran.toString();
     var kal = "";
     for (var i = 0; i < 5; i++) {
       for (var j = 0; j < 5; j++) {
