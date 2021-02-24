dc98a43f49d140983e70e6992ecc4e510afa7b54
widjajarn <widjajarn@gmail.com>
2021-02-24 16:46:21 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  public mydiv: HTMLDivElement;
+  public baris: HTMLInputElement; 
+  public kolom: HTMLInputElement; 
+
+  public giliran: number = 1; 
+  public papan = new Array(5).fill(0).map(() => new Array(5).fill("*"));
+  public name = "Angular " + VERSION.major;
+
+  ngOnInit() {
+    this.mydiv = document.getElementById("kotak") as HTMLDivElement;
+    this.baris = document.getElementById("tbaris") as HTMLInputElement;
+    this.kolom = document.getElementById("tkolom") as HTMLInputElement;
+    this.showpapan();
+  }
+
+  seticon() {
+    var brs = parseInt(this.baris.value); 
+    var klm = parseInt(this.kolom.value); 
+    this.papan[brs][klm] = this.giliran;
+    this.showpapan(); 
+  }
+
+  showpapan() {
+    var kal = "";
+    for (var i = 0; i < 5; i++) {
+      for (var j = 0; j < 5; j++) {
+        kal = kal + this.papan[i][j];
+      }
+      kal = kal + "<br>";
+    }
+
+    this.mydiv.innerHTML = kal;
+  }
+}
